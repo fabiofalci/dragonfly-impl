600cf20855f4777c6c0f5637275dad4d68a1a2ae
Fabio Falci <fabiofalci@gmail.com>
2023-09-22 09:05:03 +0100

    Stub impl of EventKeyboard

--- include/EventKeyboard.h
+++ include/EventKeyboard.h
@@ -6,15 +6,42 @@
 
 #include "Event.h"
 
+const std::string KEYBOARD_EVENT = "df::keyboard";
+
+enum EventKeyboardAction {
+    UNDEFINED_KEYBOARD_ACTION = -1,
+    KEY_PRESSED,
+    KEY_RELEASED,
+    KEY_DOWN,
+};
+
+namespace Keyboard {
+enum Key {
+    UNDEFINED_KEY = -1,
+    SPACE, RETURN, ESCAPE, TAB, LEFTARROW, RIGHTARROW, UPARROW, DOWNARROW,
+    PAUSE, MINUS, PLUS, TILDE, PERIOD, COMMA, SLASH, LEFTCONTROL,
+    RIGHTCONTROL, LEFTSHIFT, RIGHTSHIFT, F1, F2, F3, F4, F5, F6, F7, F8,
+    F9, F10, F11, F12, A, B, C, D, E, F, G, H, I , J, K, L, M, N, O, P, Q,
+    R, S, T, U, V, X, Y, Z, NUM1, NUM2, NUM3, NUM4, NUM5, NUM6, NUM7,
+    NUM8, NUM9, NUM0,
+};
+}
+
 namespace df {
 
 class EventKeyboard : public Event {
 
     private:
-        int m_keyboard_count;
+        Keyboard::Key m_key_val;
+        EventKeyboardAction m_keyboard_action;
 
     public:
         EventKeyboard();
+
+        void setKey(Keyboard::Key new_key);
+        Keyboard::Key getKey() const;
+        void setKeyboardAction(EventKeyboardAction new_action);
+        EventKeyboardAction getKeyboardAction() const;
 
 };
 
---
+++ src/EventKeyboard.cpp
@@ -0,0 +1,25 @@
+#include "EventKeyboard.h"
+
+namespace df {
+
+EventKeyboard::EventKeyboard() {
+    setType("EventKeyboard");
+}
+
+void setKey(Keyboard::Key new_key) {
+
+}
+
+Keyboard::Key EventKeyboard::getKey() const {
+    return Keyboard::Key::UNDEFINED_KEY;
+}
+
+void EventKeyboard::setKeyboardAction(EventKeyboardAction new_action) {
+
+}
+
+EventKeyboardAction EventKeyboard::getKeyboardAction() const {
+    return EventKeyboardAction::UNDEFINED_KEYBOARD_ACTION;
+}
+
+}
