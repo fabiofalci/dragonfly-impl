7177a59c5dfb653bb000583914c08d2e55f78801
Fabio Falci <fabiofalci@gmail.com>
2025-04-19 23:51:39 +0100

    Formatting and change window title

--- include/DisplayManager.h
+++ include/DisplayManager.h
@@ -17,51 +17,47 @@ const int WINDOW_HORIZONTAL_CHARS_DEFAULT = 80;
 const int WINDOW_VERTICAL_CHARS_DEFAULT = 24;
 const int WINDOW_STYLE_DEFAULT = 1024;
 const sf::Color WINDOW_BACKGROUND_COLOR_DEFAULT = sf::Color::Blue;
-const std::string WINDOW_TITLE_DEFAULT = "Dragonfly";
+const std::string WINDOW_TITLE_DEFAULT = "Dragonfly-example";
 const std::string FONT_FILE_DEFAULT = "df-font.ttf";
 
-enum Justification {
-    LEFT_JUSTIFIED,
-    CENTER_JUSTIFIED,
-    RIGHT_JUSTIFIED
-};
+enum Justification { LEFT_JUSTIFIED, CENTER_JUSTIFIED, RIGHT_JUSTIFIED };
 
 class DisplayManager : public Manager {
 
-    private:
-        DisplayManager();
-        DisplayManager(DisplayManager const&);
-        void operator=(DisplayManager const&);
-        sf::Font m_font;
-        sf::RenderWindow *m_p_window;
-        int m_window_horizontal_pixels;
-        int m_window_vertical_pixels;
-        int m_window_horizontal_chars;
-        int m_window_vertical_chars;
-        sf::Color m_window_background_color;
+  private:
+    DisplayManager();
+    DisplayManager(DisplayManager const &);
+    void operator=(DisplayManager const &);
+    sf::Font m_font;
+    sf::RenderWindow *m_p_window;
+    int m_window_horizontal_pixels;
+    int m_window_vertical_pixels;
+    int m_window_horizontal_chars;
+    int m_window_vertical_chars;
+    sf::Color m_window_background_color;
 
-    public:
-        static DisplayManager& getInstance();
+  public:
+    static DisplayManager &getInstance();
 
-        int startUp();
-        void shutDown();
+    int startUp();
+    void shutDown();
 
-        int drawCh(Vector world_pos, char ch, Color color) const;
-        int drawString(Vector world_pos, std::string str, Justification just, Color color) const;
-        int getHorizontal() const;
-        int getVertical() const;
-        int getHorizontalPixels() const;
-        int getVerticalPixels() const;
-        bool setBackgroundColor(Color new_color);
-        int swapBuffers();
-        sf::RenderWindow *getWindow() const;
+    int drawCh(Vector world_pos, char ch, Color color) const;
+    int drawString(Vector world_pos, std::string str, Justification just, Color color) const;
+    int getHorizontal() const;
+    int getVertical() const;
+    int getHorizontalPixels() const;
+    int getVerticalPixels() const;
+    bool setBackgroundColor(Color new_color);
+    int swapBuffers();
+    sf::RenderWindow *getWindow() const;
 
-        float charHeight() const;
-        float charWidth() const;
-        Vector spacesToPixels(Vector spaces) const;
-        Vector pixelsToSpaces(Vector pixels) const;
+    float charHeight() const;
+    float charWidth() const;
+    Vector spacesToPixels(Vector spaces) const;
+    Vector pixelsToSpaces(Vector pixels) const;
 };
 
-}
+} // namespace df
 
 #endif // __DISPLAY_MANAGER_H__
