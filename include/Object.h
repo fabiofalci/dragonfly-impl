ced17497a2cb1036cd82d79f5b8c2fa4391b0cb6
Fabio Falci <fabiofalci@gmail.com>
2025-04-20 15:46:33 +0100

    Add new object methods for kinematics

--- include/Object.h
+++ include/Object.h
@@ -11,33 +11,46 @@ namespace df {
 
 class Object {
 
-    private: 
-        int m_id;
-        std::string m_type;
-        Vector m_position;
-        int m_altitude;
+  private:
+    int m_id;
+    std::string m_type;
+    Vector m_position;
+    int m_altitude;
+    Vector m_direction;
+    float m_speed;
 
-    public:
-        Object();
-        virtual ~Object();
+  public:
+    Object();
+    virtual ~Object();
 
-        void setId(float new_id);
-        float getId() const;
+    void setId(float new_id);
+    float getId() const;
 
-        void setAltitude(float new_altitude);
-        int getAltitude() const;
+    void setAltitude(float new_altitude);
+    int getAltitude() const;
 
-        void setType(std::string new_type);
-        std::string getType() const;
+    void setType(std::string new_type);
+    std::string getType() const;
 
-        void setPosition(Vector new_position);
-        Vector getPosition() const;
+    void setPosition(Vector new_position);
+    Vector getPosition() const;
 
-        virtual int eventHandler(const Event *p_e);
+    void setDirection(Vector new_direction);
+    Vector getDirection() const;
 
-        virtual int draw();
+    void setSpeed(float new_speed);
+    float getSpeed() const;
+
+    void setVelocity(Vector new_velocity);
+    Vector getVelocity() const;
+
+    Vector predictPosition();
+
+    virtual int eventHandler(const Event *p_e);
+
+    virtual int draw();
 };
 
-}
+} // namespace df
 
 #endif // __OBJECT_H__
--- src/Object.cpp
+++ src/Object.cpp
@@ -9,49 +9,48 @@ Object::Object() {
     setAltitude(df::MAX_ALTITUDE / 2);
 }
 
-Object::~Object() {
-    WM.removeObject(this);
-}
+Object::~Object() { WM.removeObject(this); }
 
-void Object::setId(float new_id) {
-    m_id = new_id;
-}
+void Object::setId(float new_id) { m_id = new_id; }
 
-float Object::getId() const {
-    return m_id;
-}
+float Object::getId() const { return m_id; }
 
 void Object::setAltitude(float new_altitude) {
     if (new_altitude >= 0 && new_altitude <= df::MAX_ALTITUDE)
         m_altitude = new_altitude;
 }
 
-int Object::getAltitude() const {
-    return m_altitude;
+int Object::getAltitude() const { return m_altitude; }
+
+void Object::setType(std::string new_type) { m_type = new_type; }
+
+std::string Object::getType() const { return m_type; }
+
+void Object::setPosition(Vector new_pos) { m_position = new_pos; }
+
+Vector Object::getPosition() const { return m_position; }
+
+void Object::setDirection(Vector new_direction) { m_direction = new_direction; }
+
+Vector Object::getDirection() const { return m_direction; }
+
+void Object::setSpeed(float new_speed) { m_speed = new_speed; }
+
+float Object::getSpeed() const { return m_speed; }
+
+void Object::setVelocity(Vector new_velocity) {
+    setSpeed(new_velocity.getMagnitude());
+    new_velocity.normalize();
+    setDirection(new_velocity);
 }
 
-void Object::setType(std::string new_type) {
-    m_type = new_type;
+Vector Object::getVelocity() const {
+    getDirection().scale(getSpeed());
+    return getDirection();
 }
 
-std::string Object::getType() const {
-    return m_type;
-}
+int Object::eventHandler(const Event *p_e) { return 0; }
 
-void Object::setPosition(Vector new_pos) {
-    m_position = new_pos;
-}
+int Object::draw() { return 0; }
 
-Vector Object::getPosition() const {
-    return m_position;
-}
-
-int Object::eventHandler(const Event *p_e) {
-    return 0;
-}
-
-int Object::draw() {
-    return 0;
-}
-
-}
+} // namespace df
